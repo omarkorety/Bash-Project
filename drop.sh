9d0643d2f488850dc97458f8e4cd3b7acc0a06f9
Shereef Assem <s.assem97@gmail.com>
2022-09-01 19:23:50 +0200

    drop modified

--- drop.sh
+++ drop.sh
@@ -2,26 +2,26 @@
 #Drop Database
 
 path=pwd
-echo -p "What is the name of the database do you want to drop? " name
+read -p "What is the name of the database do you want to drop? " name
 
 while true
 do
 	if [ -d $name ]
 	then
 		
-		echo -p "Are you sure you want to drop $name?(y/n) " ans
+		read -p "Are you sure you want to drop $name?(y/n) " ans
 		while true
 		do
 			case $ans in
 				[yY]) rm -r $name 
 					break 2;;
 				[nN]) source $path/Main_Menu.sh ;; #Return to main menu
-				*) echo -p "Please enter a suitable answer(y/n) " ans
+				*) read -p "Please enter a suitable answer(y/n) " ans
 					#return to the case again
 			esac
 		done
 	else
-		echo -p "This database is not available, Please try again " name
+		read -p "This database is not available, Please try again " name
 	fi
 done
 
