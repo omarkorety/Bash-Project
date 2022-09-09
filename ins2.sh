2fdfa4d17950a8eb7ee5241123995d74831e2df3
Shereef Assem <s.assem97@gmail.com>
2022-09-09 23:47:46 +0200

    lajghldg

--- ins2.sh
+++ ins2.sh
@@ -12,31 +12,41 @@ for (( k = 1; k <=$colnum ; k++ )); do
 	coltyp=$(awk -F: -v i="$k" '{if(NR==i) print $3}' "${tname}-meta")
 	colkey=$(awk -F: -v i="$k" '{if(NR==i) print $4}' "${tname}-meta") #to know if it primary or not
 	colname=$(awk -F: -v i="$k" '{if(NR==i) print $2}' "${tname}-meta")
-	#while true ;do
-	echo  "enter ($colname)) is $coltyp" 
+	while true ;do
+	echo  "Insert in ($colname) which its datatype is $coltyp" 
 	read input
 	if [[ $coltyp == "i" ]];then 
-		while ! [[  $input =~ ^[0-9]*$ ]]; do
-			echo "invalid DataType "
-			echo -e "enter ($colname) "
+		while ! [[  $input =~ ^[0-9]+$ ]]; do
+			echo "Invalid DataType "
+			echo -e "Please enter an integer ($colname) "
  			read input
+		done
+	elif [[ $coltyp == "s" ]];then
+		while ! [[ $input =~ ^[0-9]+$ ]]; do
+			echo "Invalid DataType"
+			echo -e "Please enter a string ($colname) "
+			read input
 		done
 	fi
 	if [[ $colkey == "primary" ]]; then
 		primcol=($(awk -F: -v i="$k" '{ if (NR > 1) print $i}' $tname))
 		#while [[ true ]];do			
-			for item in "${primcol[@]}" ; do
-				if [[ $input == "$item" ]];then
-					echo "this primary key found"
+		#	for item in "${primcol[@]}" ; do
+				if cut -d: -f1  $tname | tail +2 | grep '\<${input}\>' ;then
+				#	echo "this primar
+					echo This value already exists
+					continue
 				else
 					break ;
 				fi
 				echo -e "enter ($colname)"
 			 	read input
 
-			done
+		#	done
 		#done
+		
 	fi
+	done
 #	echo -e "enter ($colname) "
 # 	read input
 	#done
